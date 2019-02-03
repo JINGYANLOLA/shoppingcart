bb196888e8c2d4b1b8e714aaa36e6faeb1c8d527
JINGYANLOLA <43939209+JINGYANLOLA@users.noreply.github.com>
2019-02-03 15:41:57 -0600

    Updates

---
+++ precache-manifest.7f890b99ab245e4a3ecf3b00c09da9e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "944445b761604a815af4",
+    "url": "/myapp/static/js/main.944445b7.chunk.js"
+  },
+  {
+    "revision": "962b017e5ee7d420674e",
+    "url": "/myapp/static/js/1.962b017e.chunk.js"
+  },
+  {
+    "revision": "944445b761604a815af4",
+    "url": "/myapp/static/css/main.c20be49b.chunk.css"
+  },
+  {
+    "revision": "67a28f923450a5efc879b8358bc4cdf7",
+    "url": "/myapp/index.html"
+  }
+];
